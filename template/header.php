bba47f53b0472d176203c023a9a4901e8424ddd6
brandon <brandonyulian10@gmail.com>
2024-11-17 19:46:36 -0500

    Version 1.0

---
+++ template/header.php
@@ -0,0 +1,50 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+        <header>
+            <div >
+                <nav>
+
+                    <div>
+                        <div class="logoNav">
+
+                            <div class="imglogo">
+                                <img src="../assets/img/LogoFR.png"  alt="Aqui va el logo SFR" width="60px" height="80px">
+                            </div>
+
+                            <div class="SENA_FAST_REGISTER">
+                                <p>FAST REGISTER</p>
+                            </div>
+                            
+                            
+                            <ul class="listaheader">
+
+                                <li class="listaMenu"><a href="index.php"><b>Inicio</b></a></li>
+                                <li class="listaMenu"><a href=""><b>|</b></a></li>
+                                <li class="listaMenu"><a href="../sobreNosotros.php"><b>Sobre Nosotros</b></a></li>
+                                <li class="listaMenu"><a href=""><b>|</b></a></li>
+                                <li class="listaMenu"><a href="../contactanos.php"><b>Contactanos</b></a></li>
+                                <li class="listaMenu"><a href=""><b>|</b></a></li>
+                                
+                            </ul>
+                            </div>
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </nav>
+            </div>
+        </header>
+    </div>
+
+    <div class="sombraNav"></div>
+</body>
+</html>

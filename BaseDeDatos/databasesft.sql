d9c600d6e55d3d77ecc956a7e9effcde18c402c1
Alejandro <alejoreyvm@hotmail.com>
2025-01-12 22:49:05 -0500

    base de datos y codigo

---
+++ BaseDeDatos/databasesft.sql
@@ -0,0 +1,334 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 13-01-2025 a las 03:42:23
+-- Versión del servidor: 8.0.30
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `databasesft`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `computador`
+--
+
+CREATE TABLE `computador` (
+  `id_serial_pc` int NOT NULL,
+  `id_fecha_entrega` int NOT NULL,
+  `id_documento` int NOT NULL,
+  `id_estado_computadores` int NOT NULL,
+  `n_ficha` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id_estado` int NOT NULL,
+  `estado` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`id_estado`, `estado`) VALUES
+(1, 'Activo'),
+(2, 'Inactivo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_computadores`
+--
+
+CREATE TABLE `estado_computadores` (
+  `id_estado_computadores` int NOT NULL,
+  `mouse` varchar(45) NOT NULL,
+  `cargador` varchar(45) NOT NULL,
+  `computador` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fecha_entrega`
+--
+
+CREATE TABLE `fecha_entrega` (
+  `id_fecha_entrega` int NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fichas`
+--
+
+CREATE TABLE `fichas` (
+  `n_ficha` int NOT NULL,
+  `nombre_formacion` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `fichas`
+--
+
+INSERT INTO `fichas` (`n_ficha`, `nombre_formacion`) VALUES
+(1, 'indefinida');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `id_marca` int NOT NULL,
+  `nombre_marca` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `objetos`
+--
+
+CREATE TABLE `objetos` (
+  `id_serial` bigint NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `caracteristica` varchar(100) NOT NULL,
+  `foto` varchar(500) DEFAULT NULL,
+  `id_marca` int NOT NULL,
+  `id_documento` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int NOT NULL,
+  `nombre_rol` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id_rol`, `nombre_rol`) VALUES
+(1, 'Administrador\r\n'),
+(2, 'Aprendiz\r\n'),
+(3, 'Funcionario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_documento` int NOT NULL,
+  `nombre_completo` varchar(50) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `contraseña` varchar(500) NOT NULL,
+  `ficha` int DEFAULT NULL,
+  `codigo_barras` bigint NOT NULL,
+  `id_estado` int NOT NULL,
+  `id_rol` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_documento`, `nombre_completo`, `correo`, `contraseña`, `ficha`, `codigo_barras`, `id_estado`, `id_rol`) VALUES
+(1106227432, 'Gabriela Devia Marin', 'gabilinses@hotmail.com', '$2y$10$URAvTspBb1YjebC8ac9NduB4E7h5pvcDzjqaODuyQabiP2OP04Dp6', 1, 1106227432, 1, 2),
+(1107978187, 'Alejandro Velandia', 'alejoreyvm@hotmail.com', '$2y$10$f7/K3UeIo.FUHyLmW3U8zu3lRynVuRnRcanh9UMSRYRVU96IcJsp6', 1, 1107978187, 1, 2),
+(1107978188, 'Brandon Yulian', 'alejoreyvm@hotmail.com', '$2y$10$mRv4ssbWFSK1txEjB76jyeoMDYoK.j3wtt7t4oxwk1ZyaB93GOeeC', 1, 1107978188, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculos`
+--
+
+CREATE TABLE `vehiculos` (
+  `id_placa` bigint NOT NULL,
+  `foto` varchar(500) DEFAULT NULL,
+  `foto_tarjeta_propiedad` varchar(500) DEFAULT NULL,
+  `id_estado` int NOT NULL,
+  `id_documento` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `computador`
+--
+ALTER TABLE `computador`
+  ADD PRIMARY KEY (`id_serial_pc`),
+  ADD KEY `id_fecha_entrega` (`id_fecha_entrega`),
+  ADD KEY `id_documento` (`id_documento`),
+  ADD KEY `id_estado_computadores` (`id_estado_computadores`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
+-- Indices de la tabla `estado_computadores`
+--
+ALTER TABLE `estado_computadores`
+  ADD PRIMARY KEY (`id_estado_computadores`);
+
+--
+-- Indices de la tabla `fecha_entrega`
+--
+ALTER TABLE `fecha_entrega`
+  ADD PRIMARY KEY (`id_fecha_entrega`);
+
+--
+-- Indices de la tabla `fichas`
+--
+ALTER TABLE `fichas`
+  ADD PRIMARY KEY (`n_ficha`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id_marca`);
+
+--
+-- Indices de la tabla `objetos`
+--
+ALTER TABLE `objetos`
+  ADD PRIMARY KEY (`id_serial`),
+  ADD KEY `id_documento` (`id_documento`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_documento`),
+  ADD KEY `ficha` (`ficha`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_estado` (`id_estado`);
+
+--
+-- Indices de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD PRIMARY KEY (`id_placa`),
+  ADD KEY `id_estado` (`id_estado`),
+  ADD KEY `id_documento` (`id_documento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id_estado` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_computadores`
+--
+ALTER TABLE `estado_computadores`
+  MODIFY `id_estado_computadores` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fecha_entrega`
+--
+ALTER TABLE `fecha_entrega`
+  MODIFY `id_fecha_entrega` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fichas`
+--
+ALTER TABLE `fichas`
+  MODIFY `n_ficha` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id_marca` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `computador`
+--
+ALTER TABLE `computador`
+  ADD CONSTRAINT `computador_ibfk_1` FOREIGN KEY (`id_fecha_entrega`) REFERENCES `fecha_entrega` (`id_fecha_entrega`),
+  ADD CONSTRAINT `computador_ibfk_2` FOREIGN KEY (`id_documento`) REFERENCES `usuario` (`id_documento`),
+  ADD CONSTRAINT `computador_ibfk_3` FOREIGN KEY (`id_estado_computadores`) REFERENCES `estado_computadores` (`id_estado_computadores`);
+
+--
+-- Filtros para la tabla `objetos`
+--
+ALTER TABLE `objetos`
+  ADD CONSTRAINT `objetos_ibfk_1` FOREIGN KEY (`id_documento`) REFERENCES `usuario` (`id_documento`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`ficha`) REFERENCES `fichas` (`n_ficha`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuario_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuario_ibfk_4` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD CONSTRAINT `vehiculos_ibfk_1` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`),
+  ADD CONSTRAINT `vehiculos_ibfk_2` FOREIGN KEY (`id_documento`) REFERENCES `usuario` (`id_documento`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
